bff266729ba5b7a6e3ab14479b3223d33ae164d0
Ranson <Ransonikuo@gmail.com>
2014-10-04 10:25:04 -1000

    finish

--- spec/challenge_1-spec.js
+++ spec/challenge_1-spec.js
@@ -14,16 +14,16 @@ describe("Sum of a Multiple", function () {
 
   // when your tests pass, uncomment the next two and
   // change the expected value to your calculated result
-  /*
-  it("should return 'x' for n=1000 (see challenge_1-spec.js)", function () {
+  
+ it("should return '233168' for n=1000 (see challenge_1-spec.js)", function () {
     var sum = challenge_1.sumOfAMultiple(1000);
-    expect(sum).toBe(0);
+    expect(sum).toBe(233168);
   });
 
-  it("should return 'x' for n=10000 (see challenge_1-spec.js)", function () {
+  it("should return '23331668' for n=10000 (see challenge_1-spec.js)", function () {
     var sum = challenge_1.sumOfAMultiple(10000);
-    expect(sum).toBe(0); // you change this value to the correct sum!
+    expect(sum).toBe(23331668); // you change this value to the correct sum!
   });
-  */
+  
   
 });    
